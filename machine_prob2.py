b294ef81bceb9b1a5f812eb1023ec15e0004bba0
Jerald Torres <115351172+AkosiJerald@users.noreply.github.com>
2023-07-06 13:48:37 +0800

    Add files via upload

---
+++ machine_prob2.py
@@ -0,0 +1,12 @@
+num1 = 5
+num2 = 3
+
+sum = num1 + num2
+mult = num1 * num2
+dive = num1 / num2
+sub = num1 - num2
+
+print("Sum is",sum)
+print("Multiply is",mult)
+print("Subtract is",sub)
+print("Divide is",dive)
